27989d855bc333b536ec943b667bb3227b48a086
silveriale <leticia.silveria.costa@gmail.com>
2025-02-17 18:27:52 -0300

    Ajustes ajustes nas páginas das bicicletas Inclusão da JavaScript ao final das páginas Inclusão de animação no menu Ajuste compra, formulário agora carrega o produto pré definido com JavaScript

---
+++ js/script.js
@@ -0,0 +1,25 @@
+//Ativar link do menu
+const links = document.querySelectorAll(".header-menu a"); // Seleciona todos os links do menu
+
+function ativarLink(link) {
+    const url = location.href;  // Pega a URL atual
+    const href = link.href;   // Pega o href do link
+    if (url.includes(href)) {  // Se a URL atual incluir o href do link
+        link.classList.add("ativo");  // Adiciona a classe ativo
+    } 
+}
+
+links.forEach(ativarLink);  // Para cada link, chama a função ativarLink
+
+// Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);  // Pega os parâmetros da URL
+
+function ativarProduto(parametro) { // Função para ativar o produto
+    const elemento = document.getElementById(parametro);  // Pega o elemento com o ID do parâmetro
+    if (elemento) {  // Se o elemento existir
+        elemento.checked = true;  // Verifica se o elemento existe
+    }
+}
+
+parametros.forEach(ativarProduto);  // Para cada parâmetro, chama a função ativarProduto
